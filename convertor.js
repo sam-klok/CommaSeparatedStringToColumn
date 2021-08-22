0b866400e52cd841fc3eacf8e4627fc7550ac94e
sam-klok <sklok11@gmail.com>
2021-08-22 12:08:19 -0700

    Initial commit

---
+++ convertor.js
@@ -0,0 +1,6 @@
+function addFewSymbols() {
+    var txtInput = document.getElementById('txtInput').innerText;
+    var newText = txtInput + " : modified text";
+    var outputElement = document.getElementById('txtOutput');
+    outputElement.innerText = newText;
+}
